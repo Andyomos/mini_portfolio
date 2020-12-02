a3f4795a30690ebea6cfb47b7c032c6288f2b48d
Anderson Omoragbon <61177055+Andyomos@users.noreply.github.com>
2020-12-02 13:50:19 -0700

    updated portolio, added tuggle background

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const myHexNumber = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
 let y;
 const hexBtn = document.getElementById("btn");
 const containers = document.getElementById("containers");
+
 const body = document.querySelector("body");
 const display = document.getElementById("display");
 // Responsive Toggle NavBar
@@ -12,6 +13,7 @@ const prevBtn = document.getElementsByClassName('prevBtn');
 const imagesContainer = document.querySelector('.images');
 let counter = 0;
 
+
 const pictures = [`url(img/Andy-${0}.jpg)`, `url(img/Andy-${1}.jpg)`, `url(img/Andy-${2}.jpg)`, `url(img/Andy-${3}.jpg)`, `url(img/Andy-${4}.jpg)`];
 
 // Portfolio Picture Slide
@@ -21,8 +23,7 @@ const nextSlide = () => {
     }
         
     imagesContainer.style.backgroundImage = `url(img/Andy-${counter}.jpg)`;
-    counter++;
-    
+    counter++;  
 }
 
 const prevSlide = () => {
@@ -40,7 +41,6 @@ const hexControl = () => {
         let random = Math.floor(Math.random() * myHexNumber.length);   
         hexHasch += myHexNumber[random];          
     }
-
     body.style.backgroundColor = hexHasch;
     containers.style.color = hexHasch;
     display.textContent = hexHasch;  
@@ -49,14 +49,14 @@ const hexControl = () => {
 // Responsive Toggle NavBar
 const navMobile = () => {
     if (y !== 1) {
-        console.log(mobileDisplay) 
         mobileDisplay.style.display = 'block';
         return y = 1;
     } else {     
-        console.log('clicked');
         mobileDisplay.style.display = 'none';
         return y = 0;
     }  
 }
+
+
 
 
